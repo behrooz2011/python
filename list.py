d02e283ef4083c121bf17c4b50f5919639e90033
Bruce Bakshi <bmmm1370@gmail.com>
2020-02-17 22:50:24 -0500

    first message

---
+++ list.py
@@ -0,0 +1,9 @@
+def skip_elements(elements):
+  arr=[]
+  for i in range(0,len(elements),2):
+   arr.append(elements[i])
+  return arr
+
+print(skip_elements(["a", "b", "c", "d", "e", "f", "g"])) # Should be ['a', 'c', 'e', 'g']
+print(skip_elements(['Orange', 'Pineapple', 'Strawberry', 'Kiwi', 'Peach'])) # Should be ['Orange', 'Strawberry', 'Peach']
+print(skip_elements([])) # Should be []
